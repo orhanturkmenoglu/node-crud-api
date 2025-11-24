dead0eea495fd77451cfd7949a6aa83df4a88a76
orhan <orhantrkmn749@gmail.com>
2025-11-25 01:13:58 +0300

    feat: initialize Express app and add root GET endpoint

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello there");
+// express: api oluşturmak için kullanılan popüler bir Node.js framework'üdür.
+
+const express = require("express");
+const app = express();
+
+// app.get metodu, belirli bir rota için GET isteklerini işler.
+// req : istemciden gelen isteği temsil eder.
+// res : sunucunun istemciye göndereceği yanıtı temsil eder.
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+})
+
+
+// listen metodu, belirli bir port üzerinde gelen istekleri dinlemeye başlar.
+app.listen(5173,()=>{
+    console.log("Server is running on port 5173");
+})
+
+
